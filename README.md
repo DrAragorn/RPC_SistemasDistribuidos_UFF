e22631735b32ac3d62e9569582358a0e22257ca2
Thiago Aragão <thiago.alpha.06@gmail.com>
2026-04-05 17:46:45 -0300

    Primeiro experimento feito

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RPC_SistemasDistribuidos_UFF
+
+Execute 'python server.py' em um terminal
+
+Execute 'python client.py localhost' em outro terminal.
--- client.py
+++ client.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
         exit('Usage {} SERVER'.format(sys.argv[0]))
     server = sys.argv[1]
     conn = rpyc.connect(server, 18861)
-    print('Conectado ao servidor')
     print(conn.root)
     print(conn.root.get_answer())
     print(conn.root.the_real_answer_though)
